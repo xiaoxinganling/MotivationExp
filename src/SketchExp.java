81f416bc96efb0f9c60abd968d89fd4d10304038
zmxxx <1214026832@qq.com>
2021-06-07 02:14:04 +0800

    sketch exp

---
+++ src/SketchExp.java
@@ -0,0 +1,360 @@
+import java.awt.*;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+
+public class SketchExp {
+    private static int maxIteration = 200000;
+    public static void main(String[] args) throws Exception {
+        Map<String, Job> res = singleJobOutDegreeExp("C:\\Users\\xiaoxinganling\\Desktop\\batch_task.csv");
+        List<String> keys = new ArrayList<>(res.keySet());
+        Collections.sort(keys, (o1, o2) -> res.get(o1).startTime.subtract(res.get(o2).startTime).intValue());
+        for(String k : keys){
+            if(res.get(k).tasks.size() == 33){
+//                Map<String, GNode> graph = getGraph(res.get(k));
+//                for(String key : graph.keySet()){
+//                    GNode gNode = graph.get(key);
+//                    System.out.println(gNode.id + " " + gNode.inDegree + " " + gNode.outDegree);
+//                }
+//                System.out.println(getEndTask(graph));
+//                for(int i = 1; i < 14; i++){
+//                    System.out.println(i + " " + getTasksWithStep(res.get(k), i, getEndTask(graph)));
+//                }
+//                for(int i = 1; i < 10; i++) {
+//                    System.out.println(i + " " + getTasksWithOutdegree(res.get(k), i));
+//                }
+//                Set<String> cache = getTasksWithOutdegree(res.get(k), 4);
+//                System.out.println(getSizeWithCache(res.get(k), cache));
+//                System.out.println(getJCTWithCache(res.get(k),cache,getEndTask(graph)));
+                Job j = res.get(k);
+                Map<String, GNode> graph = getGraph(j);
+                String endTask = getEndTask(graph);
+                BufferedWriter bw = new BufferedWriter(new FileWriter("one_job_res"));
+                // 随着出边数的增加，cache收益和内存占用的变化
+                List<Integer> decreaseTime = new ArrayList<>();
+                List<Double> memoryConsumption = new ArrayList<>();
+                for(int i = 1; i < 12; i++){
+                    Set<String> cache = getTasksWithOutdegree(j, i);
+                    decreaseTime.add(getJCTWithCache(j,cache,endTask).intValue());
+                    memoryConsumption.add(getSizeWithCache(j, cache));
+                }
+                bw.write(decreaseTime + "\n");
+                bw.write(memoryConsumption + "\n");
+                System.out.println(decreaseTime);
+                System.out.println(memoryConsumption);
+                // 随着距离action远近的增加，cache收益和内存占用的变化
+                decreaseTime = new ArrayList<>();
+                memoryConsumption = new ArrayList<>();
+                for(int i = 1; i < 12; i++){
+                    Set<String> cache = getTasksWithStep(j,i,endTask);
+                    decreaseTime.add(getJCTWithCache(j,cache,endTask).intValue());
+                    memoryConsumption.add(getSizeWithCache(j, cache));
+                }
+                System.out.println(decreaseTime);
+                System.out.println(memoryConsumption);
+                bw.write(decreaseTime + "\n");
+                bw.write(memoryConsumption + "\n");
+                bw.close();
+                return;
+            }
+        }
+        System.out.println(keys.size());
+        writeFile(keys, res, "job_description");
+    }
+    // for every job
+    // 1. get execution time with cached tasks
+    public static BigInteger getJCTWithCache(Job j, Set<String> cache, String endTask){
+        BigInteger before = BigInteger.valueOf(0);
+        for(Task t : j.tasks){
+            before = before.add(t.endTime.subtract(t.startTime));
+        }
+        Set<String> needToCalculate = new HashSet<>();
+        Queue<Task> queue = new LinkedList<>();
+        for(Task t : j.tasks){
+            if(t.taskId.equals(endTask)){
+                queue.offer(t);
+                needToCalculate.add(endTask);
+                break;
+            }
+        }
+        while(!queue.isEmpty()){
+            Task cur = queue.poll();
+            for(String parent : cur.parents) {
+                if (cache.contains(parent)) {
+                    continue;
+                }
+                for (Task t : j.tasks) {
+                    if (t.taskId.equals(parent)) {
+                        queue.offer(t);
+                        needToCalculate.add(t.taskId);
+                        break;
+                    }
+                }
+            }
+        }
+        BigInteger after = BigInteger.valueOf(0);
+        for(Task t : j.tasks){
+            //System.out.println(t.taskId + " " + needToCalculate + " " + needToCalculate.contains(t.taskId));
+            if(needToCalculate.contains(t.taskId)){
+                //System.out.println(t.endTime + " " + t.startTime + " " + t.endTime.subtract(t.startTime));
+                after = after.add(t.endTime.subtract(t.startTime));
+            }
+        }
+        //System.out.println(needToCalculate);
+        //System.out.println(before);
+        return before.subtract(after);
+    }
+
+    // 2. get tasks with outdegree
+    public static Set<String> getTasksWithOutdegree(Job j, int outDegree){
+        Set<String> res = new HashSet<>();
+        Map<String, GNode> graph = getGraph(j);
+        for(String key : graph.keySet()){
+            if(graph.get(key).outDegree == outDegree){
+                res.add(key);
+            }
+        }
+        return res;
+    }
+
+    // 3. get tasks with step
+    public static Set<String> getTasksWithStep(Job j, int step, String endTask){
+        Queue<Task> queue = new LinkedList<>();
+        for(Task t : j.tasks){
+            if(t.taskId.equals(endTask)){
+                queue.offer(t);
+                break;
+            }
+        }
+        while(step > 0 && !queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Task cur = queue.poll();
+                for(String parent : cur.parents){
+                    for(Task t : j.tasks){
+                        if(t.taskId.equals(parent)){
+                            queue.offer(t);
+                            break;
+                        }
+                    }
+                }
+            }
+            step--;
+        }
+        Set<String> res = new HashSet<>();
+        while(!queue.isEmpty()){
+            res.add(queue.poll().taskId);
+        }
+        return res;
+    }
+
+    // 4. get total size with chached tasks
+    public static double getSizeWithCache(Job j, Set<String> cache){
+        double res = 0;
+        for(Task t : j.tasks){
+            if(cache.contains(t.taskId)){
+                res += t.memorySize;
+            }
+        }
+        return res;
+    }
+
+    // 5. get graph with jobs
+    public static Map<String, GNode> getGraph(Job j){
+        Map<String, GNode> graph = new HashMap<>();
+        for(Task t: j.tasks){
+            GNode child = graph.getOrDefault(t.taskId, new GNode(0, 0, t.taskId));
+            for(String tp : t.parents){
+                GNode parent = graph.getOrDefault(tp, new GNode(0,0, tp));
+                parent.outDegree++;
+                child.inDegree++;
+                graph.put(tp, parent);
+            }
+            graph.put(t.taskId, child);
+        }
+        return graph;
+    }
+    // 6. get end task
+    public static String getEndTask(Map<String, GNode> graph){
+        for(String key : graph.keySet()){
+            if(graph.get(key).outDegree == 0){
+                return key;
+            }
+        }
+        return null;
+    }
+
+    public static Map<String, Job> singleJobOutDegreeExp(String fileName) throws Exception {
+        Map<String, Job> res = new HashMap<>();
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line;
+        int curNum = 0;
+        while ((line = br.readLine()) != null) {
+            String[] attrs = line.split(",");
+            if(attrs.length != 9){
+                System.out.println(line);
+                continue;
+            }
+            Task task = new Task(attrs[0], BigInteger.valueOf(Long.valueOf(attrs[1])),
+                    attrs[2], attrs[3], attrs[4], BigInteger.valueOf(Long.valueOf(attrs[5])),
+                    BigInteger.valueOf(Long.valueOf(attrs[6])),
+                    Double.valueOf(attrs[7]), Double.valueOf(attrs[8]));
+            Job job = res.getOrDefault(task.jobName, new Job(task.jobName));
+            job.addTask(task);
+            res.put(task.jobName, job);
+            curNum++;
+            if(curNum % 1000 == 0){
+                System.out.println(curNum + " / " + 14295731);
+            }
+            if(curNum > maxIteration){
+                break;
+            }
+        }
+        br.close();
+        return res;
+    }
+
+    private static void writeFile(List<String> keys, Map<String, Job> map, String fileName) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+        for (String key : keys){
+            Job curJob = map.get(key);
+            bw.write(curJob + " " + curJob.tasks.size() + "\n");
+            for(Task t : curJob.tasks){
+                bw.write(t + "\n");
+            }
+        }
+        bw.close();
+    }
+//    private StringBuilder readFileContent(String fileName, Map<Integer,CacheAdvice> map) throws Exception {
+//        StringBuilder sb = new StringBuilder();
+//        int cur = 0;
+//        String line = null;
+//        BufferedReader br = new BufferedReader(new FileReader(fileName));
+//        while((line = br.readLine())!=null){
+//            cur++;
+//            if(map.get(cur)!=null){
+//                CacheAdvice cache = map.get(cur);
+//                if(cache.type.equals(ITERATION)){
+////                    if(!line.contains(";")){
+////                        sb.append(line).append("\n");
+////                        while ((line = br.readLine())!=null){
+////                            cur++;
+////                            if(line.contains(";")){
+////                                break;
+////                            }
+////                            sb.append(line).append("\n");
+////                        }
+////                    }
+//                    int index = line.indexOf(';');
+//                    sb.append(line, 0, index).append(".cache()").append(line,index,line.length()).append("\n");
+//                }else{
+//                    // TODO: add more cache type
+//                    for(char ch:line.toCharArray()){
+//                        if(ch==9 || ch==32){
+//                            sb.append(ch);
+//                        }else{
+//                            break;
+//                        }
+//                    }
+//                    sb.append(cache.reason.getText()).append(".cache();\n");
+//                    sb.append(line).append("\n");
+//                }
+//            }else{
+//                sb.append(line).append("\n");
+//            }
+//        }
+//        br.close();
+//        return sb;
+//    }
+//}
+}
+class Task{
+    public String taskName;
+    public BigInteger instanceNum;
+    public String jobName;
+    public String taskType;
+    public String taskStatus;
+    public BigInteger startTime;
+    public BigInteger endTime;
+    public double cpuNeed;
+    public double memorySize;
+    public List<String> parents;
+    // List<Task> children;
+    public String taskId;//jobName_taskName => taskNum
+
+    public Task(String taskName, BigInteger instanceNum, String jobName, String taskType, String taskStatus, BigInteger startTime, BigInteger endTime, double cpuNeed, double memorySize) {
+        this.taskName = taskName;
+        this.instanceNum = instanceNum;
+        this.jobName = jobName;
+        this.taskType = taskType;
+        this.taskStatus = taskStatus;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.cpuNeed = cpuNeed;
+        this.memorySize = memorySize;
+        this.parents = new ArrayList<>();
+        generateParentsAndId();
+        //this.children = new ArrayList<>();
+    }
+    private void generateParentsAndId(){
+        String[] tasks = taskName.split("_");
+        //taskId = jobName + "_" + tasks[0].substring(1);
+        taskId = tasks[0].substring(1);
+        parents.addAll(Arrays.asList(tasks).subList(1, tasks.length));
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskName='" + taskName + '\'' +
+                ", instanceNum=" + instanceNum +
+                ", jobName='" + jobName + '\'' +
+                ", taskType='" + taskType + '\'' +
+                ", taskStatus='" + taskStatus + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", cpuNeed=" + cpuNeed +
+                ", memorySize=" + memorySize +
+                ", parents=" + parents +
+                ", taskId='" + taskId + '\'' +
+                '}';
+    }
+}
+class Job{
+    public String jobName;
+    public BigInteger startTime = BigInteger.valueOf(Integer.MAX_VALUE);
+    public BigInteger endTime = BigInteger.valueOf(-1);
+    public List<Task> tasks = new ArrayList<>();
+
+    public Job(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public void addTask(Task t){
+        tasks.add(t);
+        startTime = startTime.min(t.startTime);
+        endTime = endTime.max(t.endTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Job{" +
+                "jobName='" + jobName + '\'' +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                '}';
+    }
+}
+
+class GNode{
+    public int outDegree;
+    public int inDegree;
+    public String id;
+
+    public GNode(int outDegree, int inDegree, String id) {
+        this.outDegree = outDegree;
+        this.inDegree = inDegree;
+        this.id = id;
+    }
+}
---
+++ src/utils/TestTask.java
@@ -0,0 +1,68 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class TestTask {
+    public static void main(String[] args) throws Exception {
+        String tasks = "Task{taskName='R2_1', instanceNum=5, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[1], taskId='j_45713_2'}\n" +
+                "Task{taskName='R18_28', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86441, cpuNeed=100.0, memorySize=0.49, parents=[28], taskId='j_45713_18'}\n" +
+                "Task{taskName='J3_2_22', instanceNum=7, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.59, parents=[2, 22], taskId='j_45713_3'}\n" +
+                "Task{taskName='R19_18', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86441, cpuNeed=100.0, memorySize=0.49, parents=[18], taskId='j_45713_19'}\n" +
+                "Task{taskName='M16', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86436, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_45713_16'}\n" +
+                "Task{taskName='R10_9', instanceNum=3, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86438, cpuNeed=100.0, memorySize=0.49, parents=[9], taskId='j_45713_10'}\n" +
+                "Task{taskName='R30_29', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[29], taskId='j_45713_30'}\n" +
+                "Task{taskName='J28_11_17_26_27', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.59, parents=[11, 17, 26, 27], taskId='j_45713_28'}\n" +
+                "Task{taskName='R6_5', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.39, parents=[5], taskId='j_45713_6'}\n" +
+                "Task{taskName='M21', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86434, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_45713_21'}\n" +
+                "Task{taskName='R13_12', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[12], taskId='j_45713_13'}\n" +
+                "Task{taskName='J20_19_22', instanceNum=25, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86441, cpuNeed=100.0, memorySize=0.59, parents=[19, 22], taskId='j_45713_20'}\n" +
+                "Task{taskName='R29_28', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[28], taskId='j_45713_29'}\n" +
+                "Task{taskName='M23', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_45713_23'}\n" +
+                "Task{taskName='R4_26', instanceNum=5, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86442, cpuNeed=100.0, memorySize=0.49, parents=[26], taskId='j_45713_4'}\n" +
+                "Task{taskName='J17_15_16', instanceNum=7, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86441, cpuNeed=100.0, memorySize=0.59, parents=[15, 16], taskId='j_45713_17'}\n" +
+                "Task{taskName='R12_28', instanceNum=23, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[28], taskId='j_45713_12'}\n" +
+                "Task{taskName='R25_24', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.39, parents=[24], taskId='j_45713_25'}\n" +
+                "Task{taskName='R24_23', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.49, parents=[23], taskId='j_45713_24'}\n" +
+                "Task{taskName='R32_3_14_20_31', instanceNum=83, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[3, 14, 20, 31], taskId='j_45713_32'}\n" +
+                "Task{taskName='J31_22_30', instanceNum=25, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.59, parents=[22, 30], taskId='j_45713_31'}\n" +
+                "Task{taskName='R1_28', instanceNum=9, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.49, parents=[28], taskId='j_45713_1'}\n" +
+                "Task{taskName='J11_4_10', instanceNum=9, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.59, parents=[4, 10], taskId='j_45713_11'}\n" +
+                "Task{taskName='R15_26', instanceNum=5, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86441, cpuNeed=100.0, memorySize=0.49, parents=[26], taskId='j_45713_15'}\n" +
+                "Task{taskName='M5', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_45713_5'}\n" +
+                "Task{taskName='R22_21', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.39, parents=[21], taskId='j_45713_22'}\n" +
+                "Task{taskName='M26_25', instanceNum=5, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86441, cpuNeed=100.0, memorySize=0.3, parents=[25], taskId='j_45713_26'}\n" +
+                "Task{taskName='M9_6_8', instanceNum=3, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86438, cpuNeed=100.0, memorySize=0.49, parents=[6, 8], taskId='j_45713_9'}\n" +
+                "Task{taskName='R8_7', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.39, parents=[7], taskId='j_45713_8'}\n" +
+                "Task{taskName='M27', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_45713_27'}\n" +
+                "Task{taskName='M7', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86435, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_45713_7'}\n" +
+                "Task{taskName='R33_32', instanceNum=1, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.39, parents=[32], taskId='j_45713_33'}\n" +
+                "Task{taskName='J14_13_22', instanceNum=25, jobName='j_45713', taskType='1', taskStatus='Terminated', startTime=86434, endTime=86443, cpuNeed=100.0, memorySize=0.59, parents=[13, 22], taskId='j_45713_14'}\n";
+            String tasks2 = "Task{taskName='M4', instanceNum=1, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85072, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_4'}\n" +
+                    "Task{taskName='J10_1_7_9', instanceNum=22, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85157, endTime=86404, cpuNeed=100.0, memorySize=0.49, parents=[1, 7, 9], taskId='j_169363_10'}\n" +
+                    "Task{taskName='M6', instanceNum=1, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85100, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_6'}\n" +
+                    "Task{taskName='M2', instanceNum=187, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85099, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_2'}\n" +
+                    "Task{taskName='M1', instanceNum=343, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85076, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_1'}\n" +
+                    "Task{taskName='M3', instanceNum=4, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85089, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_3'}\n" +
+                    "Task{taskName='J8_6_7', instanceNum=3, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85101, endTime=85122, cpuNeed=100.0, memorySize=0.59, parents=[6, 7], taskId='j_169363_8'}\n" +
+                    "Task{taskName='M5', instanceNum=101, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85097, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_5'}\n" +
+                    "Task{taskName='J9_2_3_4_5_8', instanceNum=149, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85123, endTime=85156, cpuNeed=100.0, memorySize=0.79, parents=[2, 3, 4, 5, 8], taskId='j_169363_9'}\n" +
+                    "Task{taskName='M7', instanceNum=1, jobName='j_169363', taskType='1', taskStatus='Terminated', startTime=85067, endTime=85092, cpuNeed=100.0, memorySize=0.3, parents=[], taskId='j_169363_7'}\n";
+            printGraph(tasks);
+    }
+    private static void printGraph(String tasks) throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader(tasks));
+        String line;
+        while((line = br.readLine())!=null){
+            int start = line.indexOf('\'');
+            int end = line.substring(start + 1).indexOf('\'');
+            String taskName = line.substring(start + 1, start + end + 1);
+            String[] elements = taskName.split("_");
+            for(int i = 1; i < elements.length; i++){
+                System.out.println(elements[i] + "-->" + elements[0].substring(1) + ";");
+            }
+        }
+        br.close();
+    }
+}
